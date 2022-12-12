bf5be73633bcd93e327c3cfc286154602e68d888
Dyumin Slava <dyuminslava155@gmail.com>
2022-12-12 12:37:55 +0300

    rev 1.3 edit clicker

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import tkinter as tk
 import random
 import time
 
+color_bg = '#909090'
+color_text = '#E0E0E0'
+color_element = '#606060'
+
 
 class MainWin(tk.Tk):
     def __init__(self):
@@ -17,15 +21,41 @@ class MainWin(tk.Tk):
         self.resizable(True, False)
         self.minsize(200, 200)
         self.maxsize(1000, 1000)
-        self.config(bg='#464646')
+        self.config(bg=color_bg)
 
-        button1 = tk.Button(self, text='Кнопка 1', command=lambda: Clicker(self))
-        button2 = tk.Button(self, text='Кнопка 2')
-        button3 = tk.Button(self, text='Кнопка 3')
+        button1 = tk.Button(self,
+                            text='Кликер',
+                            font=('Arial', 15, 'bold'),
+                            bg=color_element,
+                            fg=color_text,
+                            width=10,
+                            command=lambda: Clicker(self),
+                            )
+        button2 = tk.Button(self,
+                            text='Парсер',
+                            font=('Arial', 15, 'bold'),
+                            bg=color_element,
+                            fg=color_text,
+                            width=10,
+                            command=lambda: Parser(self)
+                            )
+        button3 = tk.Button(self,
+                            text='Кнопка 3',
+                            font=('Arial', 15, 'bold'),
+                            bg=color_element,
+                            fg=color_text,
+                            )
+        button4 = tk.Button(self,
+                            text='Кнопка 4',
+                            font=('Arial', 15, 'bold'),
+                            bg=color_element,
+                            fg=color_text,
+                            )
 
         button1.grid(row=0, column=0, stick='we', padx=10)
         button2.grid(row=0, column=1)
         button3.grid(row=1, column=0, stick='we', padx=10)
+        button4.grid(row=1, column=1)
 
 
 class Clicker(tk.Toplevel):
@@ -34,7 +64,7 @@ class Clicker(tk.Toplevel):
         self.title('Кликер')
         self.geometry('500x500+500+300')
         self.resizable(False, False)
-        self.config()
+        self.config(bg=color_bg)
 
         self.points = 0
         self.but_params = {
@@ -45,79 +75,103 @@ class Clicker(tk.Toplevel):
             20: (40, 40, 8),
             30: (0, 0, 0)
         }
-        self.label = tk.Label(self,
-                              text='''Points: 0  Level: 0''',
-                              font=('Arial', 15),
-                              bg='#505050',
-                              )
-        #                  fg='#6e6e6e',
-        #                  anchor='n',  # w/e/n/s/se/sw/ne/nw/center
-        #                  justify='left',  # работает только для многострочного текста '''...'''
 
-        self.label.place(x=0,
-                         y=0,
-                         width=500,
-                         height=30)
+        self.label_points = tk.Label(self,
+                                     text='''Points: 0  Level: 0''',
+                                     font=('Arial', 15),
+                                     bg=color_element,
+                                     fg=color_text,
+                                     )
+        self.label_game_over = tk.Label(self,
+                                        text='''...''',
+                                        font=('Arial', 20, 'bold'),
+                                        bg='#505050',
+                                        fg=color_text
+                                        )
+        self.button_start = tk.Button(self,
+                                      text='Start',
+                                      font=('Arial', self.but_params[0][2], 'bold'),
+                                      bg=color_element,
+                                      fg=color_text,
+                                      command=self.start
+                                      )
         self.button_wrong1 = tk.Button(self,
                                        text='''Don't\nclick''',
-                                       command=self.game_over)
+                                       font=('Arial', self.but_params[0][2], 'bold'),
+                                       bg=color_element,
+                                       fg=color_text,
+                                       activebackground='#CC0000',
+                                       command=self.game_over
+                                       )
         self.button_wrong2 = tk.Button(self,
                                        text='''Don't\nclick''',
-                                       command=self.game_over)
+                                       font=('Arial', self.but_params[0][2], 'bold'),
+                                       bg=color_element,
+                                       fg=color_text,
+                                       activebackground='#CC0000',
+                                       command=self.game_over
+                                       )
         self.button = tk.Button(self,
                                 text='Click',
                                 font=('Arial', self.but_params[0][2], 'bold'),
-                                command=self.click)
+                                bg=color_element,
+                                fg=color_text,
+                                activebackground='#009900',
+                                command=self.click,
+                                )
 
-        self.button.place(x=50,
-                          y=50,
-                          width=self.but_params[0][0],
-                          height=self.but_params[0][1])
+        self.label_points.place(x=0, y=0, width=500, height=40)
+        self.button_start.place(x=200, y=200, width=100, height=100)
 
-        self.buttons_change_array = [self.button]
+        self.buttons_change_array = [self.button]  # массив кнопок, которые будут менять свое местоположение
+
+    def start(self):
+        self.button.place(x=50, y=50, width=self.but_params[0][0], height=self.but_params[0][1])
+        self.button_start.destroy()
+        self.start_time = time.time()
+        print(self.start_time)
 
     def click(self):
         self.points += 1
-        self.label['text'] = f'''Points: {self.points}  Level: {round(self.points // 5)} Time: 0'''
+        self.label_points['text'] = f'''Points: {self.points}  Level: {round(self.points // 5)}'''
 
         if self.points in self.but_params:
             if self.points == 10:
                 self.buttons_change_array.append(self.button_wrong1)
-            if self.points == 15:
+            elif self.points == 15:
                 self.buttons_change_array.append(self.button_wrong2)
-            if self.points == 30:
+            elif self.points == 30:
                 self.game_over()
                 return None
 
             for but in self.buttons_change_array:
                 but['font'] = ('Arial', self.but_params[self.points][2], 'bold')
                 but.place(x=random.randint(0, 400),
-                          y=random.randint(30, 400),
+                          y=random.randint(40, 400),
                           width=self.but_params[self.points][0],
                           height=self.but_params[self.points][1])
         else:
             for but in self.buttons_change_array:
-                but.place(x=random.randint(0, 400), y=random.randint(30, 400))
-
+                but.place(x=random.randint(0, 400), y=random.randint(40, 400))
 
     def game_over(self):
+        self.end_time = time.time()
         self.clear()
-        self.label = tk.Label(self,
-                              text=f'''GAME OVER!\n\nPoints: {self.points}\nLevel: {round(self.points // 5)}\nTime: 0''',
-                              font=('Arial', 15),
-                              bg='#505050',
-                              )
-        self.label.place(x=0,
-                         y=0,
-                         width=500,
-                         height=500)
+        self.label_game_over['text'] = f'''GAME OVER\n\nPoints: {self.points}\nLevel: {round(self.points // 5)}\nTime: {round(self.end_time - self.start_time, 3)} sec'''
+        self.label_game_over.place(x=0, y=0, width=500, height=500)
 
     def clear(self):
         for i in self.place_slaves():
             i.destroy()
 
 
-
+class Parser(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.title('Парсер')
+        self.geometry('500x500+500+300')
+        self.resizable(False, False)
+        self.config()
 
 
 if __name__ == '__main__':
@@ -137,3 +191,13 @@ if __name__ == '__main__':
     #                  pady=10
     #                  )
     # label.pack()
+
+# FFFFFF
+# E0E0E0 текст
+# C0C0C0
+# A0A0A0
+# 909090 фон
+# 808080
+# 606060 элемент
+# 404040
+# 000000
